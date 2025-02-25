a00f39dd0c6ab29ad3d7b013766e53c3bee21f9b
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-02-25 17:56:58 +0530

    Home Page Almost Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,15 +13,60 @@ const homeBanner = new Swiper(".homeBanner", {
 
 // Stone Collection Slider
 
-const swiper = new Swiper(".stoneCollections", {
+const stoneCollections = new Swiper(".stoneCollections", {
     slidesPerView: 4,
     spaceBetween: 30,
     navigation: {
         nextEl: ".stone-slider-btn.swiper-button-next",
         prevEl: ".stone-slider-btn.swiper-button-prev",
-      },
+    },
     // pagination: {
     //   el: ".stoneCollections .swiper-pagination",
     //   clickable: true,
     // },
-  });
+});
+
+// Popular Products Slider
+
+const popularProducts = new Swiper(".popularProducts", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".products-slider-btn.swiper-button-next",
+        prevEl: ".products-slider-btn.swiper-button-prev",
+    },
+    // pagination: {
+    //   el: ".stoneCollections .swiper-pagination",
+    //   clickable: true,
+    // },
+});
+
+// Blogs Slider
+
+const blogsSection = new Swiper(".homeBlogs", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".blogs-slider-btn.swiper-button-next",
+        prevEl: ".blogs-slider-btn.swiper-button-prev",
+    },
+    pagination: {
+      el: ".blogs-section .blog-pagi",
+      clickable: true,
+    },
+});
+
+// Testimonial Slider
+
+const testimonial = new Swiper(".testimonial", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".testi-slider-btn.swiper-button-next",
+        prevEl: ".testi-slider-btn.swiper-button-prev",
+    },
+    pagination: {
+      el: ".blogs-section .blog-pagi",
+      clickable: true,
+    },
+});
